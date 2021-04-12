45d24b56b32f9a65aaab9705819c6eeaed9151b5
sarahcvd <sarahmurstad@hotmail.com>
2021-04-12 21:42:36 +0200

    Added okhttp and gson to fetch from API

---
+++ app/src/main/java/no/kristiania/eksamen_androidprogrammering/Crypto.kt
@@ -0,0 +1,6 @@
+package no.kristiania.eksamen_androidprogrammering
+
+data class Crypto(
+    val `data`: List<Data>?,
+    val timestamp: Long?
+)
--- app/src/main/java/no/kristiania/eksamen_androidprogrammering/MainActivity.kt
+++ app/src/main/java/no/kristiania/eksamen_androidprogrammering/MainActivity.kt
@@ -2,11 +2,39 @@ package no.kristiania.eksamen_androidprogrammering
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.gson.GsonBuilder
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_Eksamen_Androidprogrammering)
         setContentView(R.layout.activity_main)
+
+        fetchJson()
+    }
+
+    fun fetchJson() {
+        println("Attempting to fetch JSON")
+
+        val url = "https://api.coincap.io/v2/assets"
+
+        val request = Request.Builder().url(url).build()
+
+        val client = OkHttpClient()
+        client.newCall(request).enqueue(object: Callback {
+            override fun onResponse(call: Call, response: Response) {
+                val body = response.body?.string()
+                println(body)
+
+                val gson = GsonBuilder().create()
+
+                val crypto = gson.fromJson(body, Crypto::class.java)
+            }
+            override fun onFailure(call: Call, e: IOException) {
+                println("Failed to execute request")
+            }
+        })
     }
 }

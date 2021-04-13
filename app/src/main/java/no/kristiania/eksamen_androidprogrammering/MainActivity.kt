1378805a7b971f9240ea8b66c6dbe9918a9feef3
Sarah van Dijk <54410259+Sarahcvd@users.noreply.github.com>
2021-04-13 20:42:11 +0200

    RecycleView and fetch from API (#1)
    * Added okhttp and gson to fetch from API
    
    * Added example recyclerView
    
    * Card fix + example to illustrate recycling
    
    * Changed adapter ++ successful print API
    
    * Added function to round decimals
    
    * Added cardView to crypto_row.xml

--- app/src/main/java/no/kristiania/eksamen_androidprogrammering/MainActivity.kt
+++ app/src/main/java/no/kristiania/eksamen_androidprogrammering/MainActivity.kt
@@ -2,11 +2,48 @@ package no.kristiania.eksamen_androidprogrammering
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.GsonBuilder
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_Eksamen_Androidprogrammering)
         setContentView(R.layout.activity_main)
+
+        findViewById<RecyclerView>(R.id.recyclerView_main).layoutManager = LinearLayoutManager(this)
+
+        fetchJson()
+    }
+
+
+    fun fetchJson() {
+        println("Attempting to fetch JSON")
+
+        val url = "https://api.coincap.io/v2/assets"
+
+        val request = Request.Builder().url(url).build()
+
+        val client = OkHttpClient()
+        client.newCall(request).enqueue(object: Callback {
+            override fun onResponse(call: Call, response: Response) {
+                val body = response.body?.string()
+                println(body)
+
+                val gson = GsonBuilder().create()
+
+                val crypto = gson.fromJson(body, Crypto::class.java)
+
+                runOnUiThread{
+                    findViewById<RecyclerView>(R.id.recyclerView_main).adapter = MainAdapter(crypto)
+                }
+            }
+            override fun onFailure(call: Call, e: IOException) {
+                println("Failed to execute request")
+            }
+        })
     }
 }

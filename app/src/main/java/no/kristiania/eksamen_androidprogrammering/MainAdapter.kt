54df7fb7c1fecb22bb7c1f4aa55aba027935c222
sarahcvd <sarahmurstad@hotmail.com>
2021-04-13 20:27:03 +0200

    Added cardView to crypto_row.xml

--- app/src/main/java/no/kristiania/eksamen_androidprogrammering/MainAdapter.kt
+++ app/src/main/java/no/kristiania/eksamen_androidprogrammering/MainAdapter.kt
@@ -26,9 +26,9 @@ class MainAdapter(val crypto: Crypto): RecyclerView.Adapter<CustomViewHolder>() 
         holder?.view?.findViewById<TextView>(R.id.textView_crypto_name).text = data?.name
         holder?.view?.findViewById<TextView>(R.id.textView_crypto_symbol).text = data?.symbol
         holder?.view?.findViewById<TextView>(R.id.textView_crypto_priceUsd).text =
-            data?.priceUsd?.toDouble()?.round(10)?.toBigDecimal().toString()
+            data?.priceUsd?.toDouble()?.round(2)?.toBigDecimal().toString()
         holder?.view?.findViewById<TextView>(R.id.textView_crypto_changePercent24Hr).text =
-            data?.changePercent24Hr?.toDouble()?.round(10)?.toBigDecimal().toString()
+            data?.changePercent24Hr?.toDouble()?.round(2)?.toBigDecimal().toString()
     }
 }
 

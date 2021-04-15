6bb7edf2213e0bc02b9bcc532e35c3b7d6feb9d2
Waligustav <wgbjork@gmail.com>
2021-04-15 18:36:24 +0200

    Addding transaction page

--- app/src/main/java/no/kristiania/eksamen_androidprogrammering/PortfolioActivity.kt
+++ app/src/main/java/no/kristiania/eksamen_androidprogrammering/PortfolioActivity.kt
@@ -1,11 +1,19 @@
 package no.kristiania.eksamen_androidprogrammering
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class PortfolioActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_portfolio)
+
+        val transactionBtn = findViewById<Button>(R.id.Transactions)
+        transactionBtn.setOnClickListener {
+            val intent = Intent(this, TransactionActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
